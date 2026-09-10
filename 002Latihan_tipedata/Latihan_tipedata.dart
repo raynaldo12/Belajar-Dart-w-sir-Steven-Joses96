70355eede42b432476139ead4ec99c2b86e45c96
raynaldo12 <ray.naldo.ang06@gmail.com>
2026-09-10 20:14:07 +0700

    Update Materi Pertemuan 1-3

---
+++ 002Latihan_tipedata/Latihan_tipedata.dart
@@ -0,0 +1,93 @@
+void main () {
+    // String nama = "Raynaldo";
+    // var nama1 = "Garry";
+    // int angka = 100;
+    // var angka1 = 100;
+
+
+    // String nama = 'Deka';
+    // int umur = 20;
+    // double bert = 56.5;
+    // String kota = "Pontianak";
+    // // Nama saya deka umur 28 tahun, berat 56.6 tinggal di pontianak 
+    // print("Nama Saya $nama, Umur saya $umur Tahun, Berat saya $bert, Tinggal di $kota");
+
+  // // Latihan penggunakan tipe data dan penjumllah
+  //   int angka1 = 20 ; 
+  //   double angka2 = 20.5;
+  //   double hasil = 0;
+  //   hasil = angka1 + angka2;
+  // //num, dynamic
+  // dynamic nama = "hole";
+  // dynamic angka = 23;
+
+  // bool bangunpagi = false;
+  // var bangunpagi1 = true;
+  // dynamic bangunpagi2 =false;
+
+
+//Array => dart = list, set,map
+
+// List mhs = ['Deny', 'Arwo',100, true, 20.9];
+// List<String> mhs2 = ['Deny', 'Arwo','Joko','Pra'];
+// print("Nama Saya ${mhs[1]}");
+
+// List data = ['Deny',100,'Sistem Informasi'];
+// print("Nama Saya ${data[0]}, Umur Saya ${data[1]}, Prodi ${data[2]}");
+
+// FUNGSI FUNGSI DATA 
+// List data = [];
+// data.add("Lahleh");
+// data.add(100);
+// data.add("Informatika");
+// print(data.indexOf(""));
+
+
+// Set mhs = ('Dona','Martin','Gary', 'Windy','Gary', 'Windy');
+// print("Nama Saya ${mhs.elementAt(2)}");
+
+//MAP
+// Map mhs = ("nama":"Agus", "umur":30, "prodi":"SI");
+
+
+// List data = [
+// {"nama":"kopi A", "harga":300000, "stok":100},
+// {"nama":"gula", "harga":20000, "stok":10},
+// {"nama":"cabe", "harga":10000, "stok":30},
+// {"nama":"bawang", "harga":100000, "stok":10},
+// {"nama":"bubuk teh", "harga":10000, "stok":30},
+// ];
+
+// print(data[0]);
+
+
+// List data= [];
+// Map data1 = {"nama":"kopi A", "harga":300000, "stok":100};
+// Map data2 = {"nama":"gula", "harga":20000, "stok":10};
+// Map data3 = {"nama":"cabe", "harga":10000, "stok":30};
+// Map data4 = {"nama":"bawang", "harga":100000, "stok":10};
+// Map data5 = {"nama":"bubuk teh", "harga":10000, "stok":30};
+
+// data.add(data1);
+// data.add(data2);
+// data.add(data3);
+// data.add(data4);
+// data.add(data5);
+// print(data);
+
+
+
+// Final Adalah settingan yang mutlak dan tidak bisa diganti/hanya 1 kali set
+
+// final String nama = "Dede"; // Tidak perlu nilai awal
+// nama = "Deka"; //otomatis akan eror karena sudah ada nilai awal
+
+// const String nama1 = "Sindy";//const wajib ada nilai awal
+// nama1 = "deka";
+
+
+//
+
+
+
+}
---
+++ 003ifnloop/latihan_ifnloop.dart
@@ -0,0 +1,100 @@
+void main() {
+  //if
+
+  //int nilai = 86;
+  // if (nilai > 85) {
+  //   print('nilai di atas 85');
+  // }
+
+  // if (nilai > 85) {
+  //   print('nilai di atas 85');
+  // } else {
+  //   print('nilai di bawah 85');
+  // }
+
+  // if (nilai > 85) {
+  //   print('nilai di atas 85');
+  // }
+
+  // if (nilai < 85) {
+  //   print('nilai di bawah 85');
+  // }
+
+  // switch
+  // var nilai = 'D';
+  // switch (nilai) {
+  //   case 'A';
+  //   print('Nilai di atas adalah A');
+  //   break;
+  //   case 'B';
+  //   print('Nilai di atas adalah B');
+  //   break;
+  //   case 'C';
+  //   print('Nilai di atas adalah C');
+  //   break;
+  //   default;
+  //   print('Nilai diluar pilihan');
+
+  // }
+
+//for
+//for in
+//foreach
+//while
+//dowhile
+
+  // for (var i = 0; i < 10; i++) {
+  //   print('Nilai $i');
+  // }
+
+  // List datamhs = ['Deni', 'Windy', 'Sindy', 'Micheal', 'Jonathan'];
+  // for (var i = 0; i < datamhs.length; i++) {
+  //   print('nama mhs = ${datamhs[i]}');
+
+  //   // if (i == 0) {
+  //   //   print('ini deni');
+  //   // }
+  // }
+
+  //for in
+  // List datamhs = ['Deni', 'Windy', 'Sindy', 'Micheal', 'Jonathan'];
+
+  // for (var element in datamhs) {
+  //   // if (element == 'Deni') {
+  //   //   print('ini deni');
+  //   // }
+  //   print(element);
+  // }
+
+//FOREACH
+// List datamhs = ['Deny','Windy','Sindy', 'Michael'];
+
+// datamhs.forEach((a) {
+// print(a);
+// if(a == 'Deny') {
+//   print(a);
+// }
+// });
+
+// //WHILE
+// var nilai = 0;
+// while(nilai < 5) {
+// print(nilai);
+// nilai++;
+// }
+
+// List datamhs = ['Deny','Windy','Sindy', 'Michael'];
+//var index = 0;
+//while (index < datamhs.legth) {
+//print(datamhs[index]);
+// index++;
+//}
+
+//DO WHILE
+List datamhs = ['Deny',' Windy', 'Sindy', 'Michale'];
+var index =0;
+do {
+  print(datamhs[index]);
+  index++;
+}while (index < datamhs.length);
+}
