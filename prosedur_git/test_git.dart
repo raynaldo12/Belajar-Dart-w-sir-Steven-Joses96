6101cfeaaf637cb7f31fa6843a891beebbb24c3f
raynaldo12 <ray.naldo.ang06@gmail.com>
2026-09-10 20:15:04 +0700

    Fix prosedur_git jadi folder biasa, bukan submodule

---
+++ prosedur_git/test_git.dart
@@ -0,0 +1,18 @@
+// //Upload Pertama Kali
+// git init 
+// git add .
+// git commit -m "Versi 1"
+
+// JIKA AUTOR TIDAK DIKENAL
+
+//git config --global user.email "a@gmail.com"
+//git config --global user.name "a-code"
+//git remote add origin (linkrepo) 
+//git branch -m main 
+//git push -u origin main
+
+//UPDATE PROGRAM DI GITHUB
+//git add .
+//git commit -m "Versi sekian"
+//git push
+//d
